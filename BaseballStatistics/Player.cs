a30a95d2d7b5bec61acfe352de9deb052d499aae
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-10 12:48:27 -0500

    intial commit

---
+++ BaseballStatistics/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballStatistics
+{
+    class Player
+    {
+        string name;
+        double[] stats;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public double calculateSluggingPercentage()
+        {
+            double hits = 0;
+            double totalBases = 0;
+
+            for (int i = 0; i < stats.Length; i++)
+            {
+                hits += stats[i];
+                totalBases += stats[i] * (i + 1);
+            }
+            return (hits / totalBases);
+        }
+
+        public void AddPlayerStats()
+        {
+            stats = new double[] { 40, 13, 5, 16 };
+        }
+
+    }
+}
---
+++ BaseballStatistics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player;
+
+            Console.WriteLine("Enter a baseball player's name:");
+            player = new Player(Console.ReadLine());
+            player.AddPlayerStats();
+            Console.WriteLine(player.calculateSluggingPercentage());
+            Console.ReadLine();
+                           
+
+    }
+    }
+}
